ece1f42bc9e39b45f5cf5f9083241b1e84ab0f95
insmyanmar <insmyanmar@hotmail.com>
2024-10-08 15:20:53 +0630

    ammstart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {'path':'home','title':'Home',component:HomeComponent},
+    {'path':'about','title':'About',component: AboutComponent},
+    {'path':'test','title':'test',component: TestComponent},
+    { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    
+];
